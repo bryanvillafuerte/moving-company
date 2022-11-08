6b4ddda02a49fe0ce611aba2d0f0503137941c48
Bryan Villafuerte <bryan.villafuerte@telenor.no>
2022-11-08 15:01:43 +0100

    Initial commit

---
+++ components/OrderCard.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "OrderCard"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+      orders: []
+    }
+  },
+  async fetch() {
+    this.orders = await fetch(
+      'http://localhost:3001/orders'
+    ).then(res => res.json())
+  }
+}
+</script>
